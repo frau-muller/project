e2b44c416193e71a0481bf7bfa65d4a73dd608c3
frau-muller <frau-muller@tut.by>
2021-10-08 00:49:44 +0300

    models

--- history/myapp/models.py
+++ history/myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+ class Questions(models.Model):
+     title = models.CharField(max_lenght=255)
+     content = models.TextField(blank=True)
+     created_at = models.DateTimeField(auto_now_add=True)
+     photo = models.ImageField(upload_to='photos/%y/%m/%d/')
+
--- history/myapp/views.py
+++ history/myapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home(request):
+    return HttpResponse('<h1>Hello world<h1>')
+
+def test(request):
+    return HttpResponse('<h1>Test Page</h1>')
