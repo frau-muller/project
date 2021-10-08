3fb7a015706e1d4ee00015b1a677d9c4921788bb
frau-muller <frau-muller@tut.by>
2021-10-08 23:03:48 +0300

    sqlite

--- history/myapp/models.py
+++ history/myapp/models.py
@@ -1,11 +1,7 @@
 from django.db import models
 
-# Create your models here.
-
-
- class Questions(models.Model):
-     title = models.CharField(max_lenght=255)
-     content = models.TextField(blank=True)
-     created_at = models.DateTimeField(auto_now_add=True)
-     photo = models.ImageField(upload_to='photos/%y/%m/%d/')
-
+class Questions(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
